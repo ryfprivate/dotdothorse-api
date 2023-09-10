5a77810643c4b1880f70df61375037bbb76ecb04
Ray Feng <ray@dotdothorse.com>
2023-09-10 18:00:40 +1000

    Fix for api error

--- server.js
+++ server.js
@@ -1,46 +1,47 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const ytdl = require('ytdl-core');
+const ytdl = require("ytdl-core");
 
 // origins: ['https://dotdothorse.com', 'https://games.dotdothorse.com']
 const corsOptions = {
-    origin: true,
-    optionsSuccessStatus: 200
-}
+  origin: true,
+  optionsSuccessStatus: 200,
+};
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get('/', (req, res) => {
-    res.send("dotdothorse youtube audio api");
-})
+app.get("/", (req, res) => {
+  res.send("dotdothorse youtube audio api");
+});
 
 const ytdlOptions = {
-    quality: 'lowestaudio'
-}
+  quality: "lowestaudio",
+};
 
-app.get('/youtube/:videoId', cors(), (req, res) => {
-    const videoId = req.params.videoId;
-    // ytdl(`https://www.youtube.com/watch?v=${videoId}`, {
-    //     quality: 'lowestaudio'
-    // }).on('error', (err) => res.status(500).send(err))
-    //     .pipe(res);
-    try {
-        ytdl(`https://www.youtube.com/watch?v=${videoId}`, ytdlOptions)
-            .on('error', error => {
-                //console.log('error: ', err)
-                throw error
-            })
-            .on('response', response => {
-                // Passing the content length header along
-                res.setHeader("content-length", response.headers["content-length"])
-            })
-            .pipe(res);
-    } catch (exception) {
-        res.status(500).send(exception);
-    }
-})
+app.get("/youtube/:videoId", cors(), (req, res) => {
+  const videoId = req.params.videoId;
+  // ytdl(`https://www.youtube.com/watch?v=${videoId}`, {
+  //     quality: 'lowestaudio'
+  // }).on('error', (err) => res.status(500).send(err))
+  //     .pipe(res);
+  //res.send(`${videoId}`);
+  try {
+    ytdl(`https://www.youtube.com/watch?v=${videoId}`, ytdlOptions)
+      .on("error", (error) => {
+        console.log("error: ", error);
+        throw error;
+      })
+      .on("response", (response) => {
+        // Passing the content length header along
+        res.setHeader("content-length", response.headers["content-length"]);
+      })
+      .pipe(res);
+  } catch (exception) {
+    res.status(500).send(exception);
+  }
+});
 
 module.exports = app;
